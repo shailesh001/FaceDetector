e8e34891d4393634f444a7612c524b8d93e99915
Shailesh Patel <shailesh001@mac.com>
2020-12-14 10:57:45 +0000

    Initial Commit

---
+++ FaceDetector/FaceDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceDetectorApp.swift
+//  FaceDetector
+//
+//  Created by Shailesh Patel on 14/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct FaceDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
